d88f5c8dfa5f5aa114732a7cb22d8c5bb8586753
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-24 00:29:06 +0530

    Added password view icon

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -6,13 +6,23 @@ import {
     KeyIcon,
     ExclamationCircleIcon,
 } from "@heroicons/react/24/outline";
-import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import {
+    ArrowRightIcon,
+    EyeIcon,
+    EyeSlashIcon,
+} from "@heroicons/react/20/solid";
 import { Button } from "./button";
 import { useFormState, useFormStatus } from "react-dom";
 import { authenticate } from "@/app/lib/usersDB";
+import { useState } from "react";
 
 export default function LoginForm() {
     const [code, action] = useFormState(authenticate, undefined);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevState) => !prevState);
+    };
     return (
         <form action={action} className="space-y-3">
             <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -50,13 +60,25 @@ export default function LoginForm() {
                             <input
                                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                                 id="password"
-                                type="password"
+                                // type="password"
+                                type={showPassword ? "text" : "password"}
                                 name="password"
                                 placeholder="Enter password"
                                 required
                                 minLength={6}
                             />
                             <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            <button
+                                type="button"
+                                onClick={togglePasswordVisibility}
+                                className="absolute right-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"
+                            >
+                                {showPassword ? (
+                                    <EyeSlashIcon className="h-5 w-5" />
+                                ) : (
+                                    <EyeIcon className="h-5 w-5" />
+                                )}
+                            </button>
                         </div>
                     </div>
                 </div>
