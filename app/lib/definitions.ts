a4f1a79d8f108e6c8cb1de4da9c0464fbabc81b2
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-26 22:36:55 +0530

    created customers type

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -9,3 +9,27 @@ export type Users = {
     updated_at: string;
     deleted_at: string;
 };
+
+export type Customers = {
+    id: number;
+    first_name: string;
+    middle_name: string;
+    last_name: string;
+    gender: "male" | "female" | "other";
+    dob: string;
+    mobile_no: string;
+    email: string;
+    address: string;
+    uan_no: string;
+    uan_password: string;
+    aadhaar_no: string;
+    pan_no: string;
+    bank_acc_no: string;
+    bank_name: string;
+    bank_branch_name: string;
+    bank_ifsc_code: string;
+    remark: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string;
+};
